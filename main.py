a7defefc77755624200813eaca4dbdb6df015dc5
FedericoPonzi <isaacisback92@gmail.com>
2017-05-07 15:49:54 +0200

    Various fix, refactoring, and adds a 'usable' autobus command.

--- main.py
+++ main.py
@@ -38,12 +38,10 @@ class State:
         self.statesDict[chat_id] = s
 
 class BotResponse(object):
-    def __init__(self, isGood, message, additional_data=None, secondary_message="")
-        self.isGood = isGood
+    def __init__(self, isSuccess, message, data=None):
+        self.isSuccess = isSuccess
         self.message= emojize(message, use_aliases = True)
-        self.additional_data = None
-        self.secondary_message = secondary_message
-
+        self.data = data
 
 class AtacBot(object):
 
@@ -60,56 +58,52 @@ class AtacBot(object):
         self.auth_server = Server(urljoin(atac_api, "autenticazione/" + "1"))
         self.paline_server = Server(urljoin(atac_api, "paline/" + "7"))
         self.percorso_server = Server(urljoin(atac_api, "percorso/" + "2"))
+        self.server_resp_codes = {"expired_session" : 824}
 
         self.__updateToken()
-        self.generic_error = emojize("Ho incontrato un errore :pensive: forse atac non è online al momento :worried:. Riprova fra poco!", use_aliases=True)
+        self.generic_error = BotResponse(False, "Ho incontrato un errore :pensive: forse atac non è online al momento :worried:. Riprova fra poco!")
 
     def __updateToken(self):
         logger.info("Logging on atac's api")
         self.token = self.auth_server.autenticazione.Accedi(self.atac_api_key, "")
 
-    def get_linee_from_palina(self, id_palina):
-        #Questo metodo restituisce l'elenco delle linee che transitano per la palina id_palina, con le relative informazioni (monitorata, abilitata)
-        return self.paline.PalinaLinee(self.token, id_palina)
+    def get_percorso_info(self, id_percorso):
+        try:
+            res = self.paline_server.paline.Percorso(self.token, str(id_percorso), "" ,"", "it")
+        except Fault as e:
+            if e.faultCode == self.server_resp_codes['expired_session']:
+                self.__updateToken()
+                return self.get_percorso_info(id_percorso)
+            else:
+                logger.error("get_percorso_info error:", e)
+                return self.generic_error
+        res = res['risposta']
+        m = "Id linea: " + str(id_percorso)
+        logger.info(res['fermate'])
+        for i in res['fermate']:
+            m += i['nome_ricapitalizzato']
 
-    def get_prossima_partenza(self, id_percorso):
-         #atac.paline_server.paline.ProssimaPartenza(atac.token, "1978", "it")
-         #{'id_richiesta': 'd90771ab7defe73b6cf5620a428d3cbb', 'risposta': '2017-05-07 13:05:00'}
-         return self.paline_server.paline.ProssimaPartenza(self.token, id_percorso, "it")
+            if "veicolo" in i:
+                m+= ":bus:"
+            m+="\n"
+        return BotResponse(True, m)
 
     def get_autobus_info(self, autobus):
         try:
             res = self.paline_server.paline.Percorsi(self.token, str(autobus), "it")
         except Fault as e:
-            raise e
-        m = "Linea " + autobus
-        res = res['risposta']
-        if res["monitorata"] == 1 and res["abilitata"]:
-            m+="Linea monitorata e abilitata per ricevere informazioni sugli orari!"
-        else:
-            return (False, "La linea non è monitorata :worried:")
-        m+="In che direzione stai andando?"
-        return (True, m, res['percorsi'])
-
-    def get_percorso(self, fr, to):
-        opt = { "mezzo" : 1, "piedi" : 1, "bus": True,
-            "metro" : True, "ferro" : True, "carpooling": False,
-            "max_distanza_bici" : 0, "linee_escluse" : [],
-            "quando" : 0
-        }
-        try:
-            res = self.percorso_server.percorso.Cerca(
-                self.token, fr, to, opt,
-                datetime.now().strftime("%Y-%m-%d %X"), "it")
-        except Fault as e:
-            logger.warn("Reauthenticating on atac api..")
-            if e.faultCode == 824:
+            if e.faultCode == self.server_resp_codes['expired_session']:
                 self.__updateToken()
-                m = self.get_percorso()
-            else:
-                m = self.generic_error
-            return (False, m)
-        return (True, res)
+                return self.get_autobus_info(autobus)
+            raise e
+        res = res['risposta']
+        m = "Linea " + autobus + "\n"
+        if res["monitorata"] == 1 and res["abilitata"]:
+            m+="Linea monitorata e abilitata per ricevere informazioni sugli orari!\n"
+        else:
+            return BotResponse(False, "La linea non è monitorata :worried:")
+        m+="In che direzione stai andando?"
+        return BotResponse(True, m, res['percorsi'])
 
     def get_autobus_from_fermata(self, id_palina):
         """ @Return: (bool success, string message)
@@ -121,11 +115,14 @@ class AtacBot(object):
             res = self.paline_server.paline.Previsioni(self.token, str(id_palina), 'it')
         except Fault as e:
             if e.faultCode == 803:
-                m = emojize("Fermata Palina inesistente :persevere: Riprova a scrivermi la palina!")
+                m = BotResponse(False, "Fermata Palina inesistente :persevere: Riprova a scrivermi la palina!")
+            elif e.faultCode == self.server_resp_codes['expired_session']:
+                self.__updateToken()
+                return self.get_autobus_from_fermata(id_palina)
             else:
-                m = self.generic_error
                 logger.error("Errore get_autobus_from_fermata richiesta palina ", id_palina, ", errore:", e)
-            return (False, m)
+                m = self.generic_error
+            return m
         m = res['risposta']['collocazione'] + "\n"
         inArrivo = res['risposta']['arrivi']
         if len(inArrivo) > 0:
@@ -135,19 +132,47 @@ class AtacBot(object):
                 m += i['annuncio'].replace("'", " minuti")
                 m += "\n"
         else:
-            return (False, emojize("Non ci sono informazioni su autobus in arrivo :persevere:", use_aliases=True))
-        return (True, emojize(m, use_aliases=True))
+            BotResponse(False, "Non ci sono informazioni su autobus in arrivo :persevere:")
+        return BotResponse(True, m)
+
+    ###Unused:
+    def get_percorso(self, fr, to):
+        opt = { "mezzo" : 1, "piedi" : 1, "bus": True,
+            "metro" : True, "ferro" : True, "carpooling": False,
+            "max_distanza_bici" : 0, "linee_escluse" : [],
+            "quando" : 0
+        }
+        try:
+            res = self.percorso_server.percorso.Cerca(
+                self.token, fr, to, opt,
+                datetime.now().strftime("%Y-%m-%d %X"), "it")
+            m = BotResponse(True, res)
+        except Fault as e:
+            if e.faultCode == self.server_resp_codes['expired_session']:
+                self.__updateToken()
+                m = BotResponse(False, lf.get_percorso())
+            else:
+                m = self.generic_error
+        return m
+
+    def get_linee_from_palina(self, id_palina):
+        #Questo metodo restituisce l'elenco delle linee che transitano per la palina id_palina, con le relative informazioni (monitorata, abilitata)
+        return self.paline.PalinaLinee(self.token, id_palina)
+
+    def get_prossima_partenza(self, id_percorso):
+         #atac.paline_server.paline.ProssimaPartenza(atac.token, "1978", "it")
+         #{'id_richiesta': 'd90771ab7defe73b6cf5620a428d3cbb', 'risposta': '2017-05-07 13:05:00'}
+         return self.paline_server.paline.ProssimaPartenza(self.token, id_percorso, "it")
+
 
 
 ## Statics (for now):
 atac = AtacBot(os.environ['ATAC_API_KEY'])
 states = State()
 
-
 ######
 ###Commands :
 ######
-
 @run_async
 def echo(bot, update):
      if states.getState(update.message.chat_id) == State.FERMATA:
@@ -159,17 +184,35 @@ def echo(bot, update):
 @run_async
 def callback_query_handler(bot, update):
     logger.info("Called callback_query_handler")
-    states.removeState(update.callback_query.message.chat_id)
+    c_id = update.callback_query.message.chat_id
+    if states.getState(c_id) == State.FERMATA:
+        query = update.callback_query
+        keyboard = [[InlineKeyboardButton("Aggiorna", callback_data=query.data)]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        req = atac.get_autobus_from_fermata(query.data)
+        if req.isSuccess:
+            m = "\nAggiornate alle " + str(datetime.now().strftime("%X"))
+            bot.editMessageText(text=req.message + m,
+                                chat_id=query.message.chat_id,
+                                message_id=query.message.message_id,
+                                reply_markup=reply_markup)
+        else:
+            update.message.reply_text(req.message)
+    elif states.getState(c_id) == State.LINEA:
+        query = update.callback_query
+        keyboard = [[InlineKeyboardButton("Aggiorna", callback_data=query.data)]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        req = atac.get_percorso_info(query.data)
+        if req.isSuccess:
+            m = "\nAggiornate alle " + str(datetime.now().strftime("%X"))
+            bot.editMessageText(text=req.message + m,
+                                chat_id=query.message.chat_id,
+                                message_id=query.message.message_id,
+                                reply_markup=reply_markup)
+        else:
+            update.message.reply_text(req.message)
 
-    query = update.callback_query
-    keyboard = [[InlineKeyboardButton("Aggiorna", callback_data=query.data)]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    m = "\nAggiornate alle " + str(datetime.now().strftime("%X"))
-    bot.editMessageText(text=atac.get_autobus_from_fermata(query.data)[1] + m, #should never fail c:
-                        chat_id=query.message.chat_id,
-                        message_id=query.message.message_id,
-                        reply_markup=reply_markup
-    )
+    states.removeState(update.callback_query.message.chat_id)
 
 @run_async
 def start_ch(bot, update):
@@ -193,11 +236,11 @@ def fermata_ch(bot, update, args):
     keyboard = [[InlineKeyboardButton("Aggiorna", callback_data=str(id_palina))]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     req = atac.get_autobus_from_fermata(id_palina)
-    if req[0]: #Se la richiesta è andata a buon fine.
-        update.message.reply_text(req[1], reply_markup=reply_markup)
+    if req.isSuccess: #Se la richiesta è andata a buon fine.
+        update.message.reply_text(req.message, reply_markup=reply_markup)
     else:
         states.setState(update.message.chat_id, State.FERMATA)
-        update.message.reply_text(req[1])
+        update.message.reply_text(req.message)
 
 @run_async
 def autobus_ch(bot, update, args):
@@ -210,13 +253,14 @@ def autobus_ch(bot, update, args):
         update.message.reply_text("Usa /autobus numeroautobus per favore.")
         return
     req = atac.get_autobus_info(id_autobus)
-    if req[0]:
-        keyboard = [[InlineKeyboardButton(direzione['capolinea'] , callback_data=direzione['id_percorso'])] for direzione in req[2]]
+    if req.isSuccess:
+        keyboard = [[InlineKeyboardButton(direzione['capolinea'] , callback_data=direzione['id_percorso'])] for direzione in req.data]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        update.message.reply_text(req[1], reply_markup=reply_markup)
-    else:
         states.setState(update.message.chat_id, State.LINEA)
-        update.message.reply_text(req[1])
+        update.message.reply_text(req.message, reply_markup=reply_markup)
+    else: #Errore case, hide the reply markup
+        states.setState(update.message.chat_id, State.LINEA)
+        update.message.reply_text(req.message)
     #update.message.reply_text("Work in progress. In futuro ti darò informazioni sulle posizioni degli autobus.")
 
 @run_async
@@ -228,8 +272,9 @@ def help_ch(bot, update):
         TrasportiRomaBot ti darà informazioni sugli autobus a Roma!
  I comandi supportati sono:
  /start per iniziare il bot
- /fermata per sapere quali autobus sono in arrivo
- /autobus per sapere dove si trova un autobus
+ /fermata quali autobus sono in arrivo
+ /autobus dove si trova un autobus
+ /prossimo fra quanto parte il prossimo autobus
     ''')
 
 @run_async
