be4e108b1653c717e0feba6b6ea3b026b24f349d
Federico Ponzi <me@fponzi.me>
2021-01-27 07:48:50 +0000

    pep-8 reformat

--- bot/state.py
+++ bot/state.py
@@ -1,15 +1,19 @@
 class State:
     """Used to keep state of the conversation."""
     FERMATA = 0
-    LINEA   = 1
+    LINEA = 1
+
     def __init__(self):
         self.statesDict = {}
+
     def getState(self, chat_id):
         if chat_id in self.statesDict:
             return self.statesDict[chat_id]
         return None
+
     def removeState(self, chat_id):
         if chat_id in self.statesDict:
             del self.statesDict[chat_id]
+
     def setState(self, chat_id, s):
         self.statesDict[chat_id] = s
